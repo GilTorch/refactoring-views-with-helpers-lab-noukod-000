432ad503ee44dce8b772ba62818e93c857818ffb
Gilbert Torchon <gilbert@noukod.com>
2019-01-28 21:03:20 +0000

    Automatically backed up by Learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -2,7 +2,7 @@ module SongsHelper
   def display_artist(song)
     if song.artist.nil?
       link_to "Add Artist",edit_song(song)
-    else 
+    else
       link_to song.artist.name,artist_path(song.artist)
     end
   end
